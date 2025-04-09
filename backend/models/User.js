31495043747a02f0d3f74acc0cb916f9678a049a
aryan10092 <aryan9090900@gmail.com>
2025-04-09 19:41:56 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -25,9 +25,8 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
-});
+})
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
@@ -40,7 +39,7 @@ userSchema.pre('save', async function(next) {
   }
 });
 
-// Method to compare password
+
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
