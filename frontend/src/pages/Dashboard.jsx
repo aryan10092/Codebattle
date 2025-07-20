eb8d5d4b33c1269e5613d483e2cbf54be686c872
ARYAN GUPTA <aryan9090900@gmail.com>
2025-07-20 18:14:42 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -689,7 +689,7 @@ const formatTime = (seconds) => {
             <Card className="bg-black border-purple-500/20">
               <CardHeader className="pb-2">
                 <div className="flex justify-between items-center">
-                  <CardTitle className="text-lg text-blue-200">{currentChallenge?.Title || 'Loading...'}</CardTitle>
+                  <CardTitle className="text-lg text-blue-200">{currentChallenge?.Title || 'Code'}</CardTitle>
                   <Badge variant="outline" className="border-gray-700 bg-purpl text-blue-200">
                     {difficulty.toUpperCase()}
                   </Badge>
@@ -698,7 +698,7 @@ const formatTime = (seconds) => {
               <CardContent>
                 <pre className="text-sm text-blue-200 whitespace-pre-wrap bg-[#1a1a2e] p-4 rounded-lg overflow-auto 
                 max-h-40 border border-gray-500/20">
-                  {currentChallenge?.Description || 'Loading challenge...'}
+                  {currentChallenge?.Description || 'Generate a challenge'}
                 </pre>
               </CardContent>
             </Card>
